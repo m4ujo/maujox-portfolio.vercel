64241eb427656fcae5adc4dda9246d0a7f32cce8
maujox <mauriciocast11@outlook.com>
2023-03-03 18:55:00 -0500

    Update Button structure

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,5 @@
-export default function Button ({ type = '', href = '#', children }) {
-  return (
-    <a className={'Button' + type} href={href}>
-      {children}
-    </a>
-  )
+export default function Button ({ type = 'a', style = '', href = '#', children }) {
+  return (type === 'a')
+    ? <a className={'Button' + style} href={href}>{children}</a>
+    : <button className={'Button' + style} type='submit'>{children}</button>
 }
