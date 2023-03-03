fa8c3bd0c96eccf961292f4583564b2be3e72c07
maujox <mauriciocast11@outlook.com>
2023-03-02 22:53:02 -0500

    Add projects data

---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    id: 1,
+    name: 'portfolio',
+    homepage: 'https://maujox.com',
+    htmlUrl: 'https://github.com/maujox/portfolio'
+  },
+  {
+    id: 2,
+    name: 'ndt-landing',
+    homepage: 'https://ndt-landing.vercel.app',
+    htmlUrl: 'https://github.com/maujox/ndt-landing'
+  },
+  {
+    id: 3,
+    name: 'edie-homepage-dcio',
+    homepage: 'https://edie-homepage-dcio.vercel.app/',
+    htmlUrl: 'https://github.com/maujox/edie-homepage-dcio'
+  },
+  {
+    id: 4,
+    name: 'tasks-localstorage',
+    homepage: 'https://tasks-localstorage.vercel.app/',
+    htmlUrl: 'https://github.com/maujox/tasks-localstorage'
+  },
+  {
+    id: 5,
+    name: 'logical-expression-solver',
+    homepage: 'https://github.com/maujox/logical-expression-solver/blob/main/README.md',
+    htmlUrl: 'https://github.com/maujox/logical-expression-solver'
+  }
+]
