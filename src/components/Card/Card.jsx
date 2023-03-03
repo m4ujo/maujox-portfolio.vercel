79c15541cb03519883b165233eeb1641e87791ea
maujox <mauriciocast11@outlook.com>
2023-03-02 22:52:27 -0500

    Add Card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import Button from '../Button/Button'
+
+export default function Card ({ name, homepage, htmlUrl }) {
+  return (
+    <div className='Card'>
+      <div className='Card-content'>
+        <div className='Card-top'>
+          <h2 className='Card-h2 Card-projectTitle'>{name}</h2>
+          <i className='bi bi-arrows-angle-expand' />
+        </div>
+        <div className='Card-bottom'>
+          <Button href={homepage} type='.line.blue'>Preview</Button>
+          <Button href={htmlUrl} type='.line.red'>Github</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
