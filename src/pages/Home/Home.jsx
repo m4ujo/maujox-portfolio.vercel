0a40ebf732ce01fcef920c9d465856067e20c05d
maujox <mauriciocast11@outlook.com>
2023-03-02 22:47:54 -0500

    Add Home page structure

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import Button from './../../components/Button/Button'
+
+export default function Home () {
+  return (
+    <div className='Home' id='Home'>
+      <h1 className='Home-h1 Home-name'>
+        Mauricio Castillo
+      </h1>
+      <p className='Home-p Home-jobName'>
+        <span>Self-taught</span>
+        <span>Programmer</span>
+      </p>
+      <div className='Home-buttons'>
+        <Button href='#contact' type='.fill.blue'>Contact</Button>
+        <Button href='#' type='.line.red'>Download CV</Button>
+      </div>
+    </div>
+  )
+}
