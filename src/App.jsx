6014049bb4dee0ad66cfc1957a51e6e879d7adee
maujox <mauriciocast11@outlook.com>
2023-03-02 22:54:20 -0500

    Update App.jsx with new added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
+import Home from './pages/Home/Home'
+import Portfolio from './pages/Portfolio/Portfolio'
 
 function App() {
   return (
     <div className="App">
+      <Home />
+      <Portfolio />
     </div>
   )
 }
