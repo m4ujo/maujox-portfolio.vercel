9fa37445bee7c266d028ff6e0f89b6fe534711f8
maujox <mauriciocast11@outlook.com>
2023-03-03 19:11:16 -0500

    Add social icons section

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -8,13 +8,28 @@ function Contact () {
         <h2 className='Contact-title'>Let<span>'</span>s talk about working together<span>...</span></h2>
         <div className='Contact-formContainer'>
           <form action='https://formsubmit.co/your@email.com' className='Contact-form' method='POST'>
-            <Input type='text' placeholder='Full name' name='fullname' />
-            <Input type='email' placeholder='Email address' name='email' />
-            <Input type='text' placeholder='Subject' name='subject' />
-            <textarea name='Message' placeholder='Message' id='Message' />
-            <Button type='btn' style='.fill.blue'>
-              Send <i className='bi bi-send-fill' />
-            </Button>
+            <div className='Contact-formRow'>
+              <Input type='text' placeholder='Full name' name='fullname' />
+              <Input type='email' placeholder='Email address' name='email' />
+            </div>
+            <div className='Contact-formRow'>
+              <Input type='text' placeholder='Subject' name='subject' />
+            </div>
+            <div className='Contact-formRow'>
+              <textarea name='Message' placeholder='Message' id='Message' />
+            </div>
+            <div className='Contact-formRow'>
+              <div className='Contact-socialIcons'>
+                <i className='bi bi-whatsapp' />
+                <i className='bi bi-instagram' />
+                <i className='bi bi-twitter' />
+                <i className='bi bi-linkedin' />
+              </div>
+              <Button type='btn' style='.fill.blue'>
+                Send <i className='bi bi-send-fill' />
+              </Button>
+            </div>
+
           </form>
         </div>
       </div>
