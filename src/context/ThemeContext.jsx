a4e71c5602e36ac079516d687a86b2007b33e3d9
maujox <mauriciocast11@outlook.com>
2023-03-11 16:51:14 -0500

    Add localstorage persistence

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,9 +1,16 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 export const ThemeContext = createContext()
 
 export function ThemeContextProvider ({ children }) {
-  const [theme, setTheme] = useState('')
+  let initialValueTheme = ''
+  if (window.localStorage.getItem('theme-data')) { initialValueTheme = window.localStorage.getItem('theme-data') }
+
+  const [theme, setTheme] = useState(initialValueTheme)
+
+  useEffect(() => {
+    window.localStorage.setItem('theme-data', theme)
+  }, [theme])
 
   const toggleTheme = () => {
     setTheme(theme === '' ? 'light' : '')
